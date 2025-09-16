a931c4425bcc63d5e74835e605323d7f2dab41ec
petricdarius <badlion30@gmail.com>
2025-09-16 22:55:54 +0300

    Implement user authentication with signup and login functionality, including user model and token generation

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { login, singup, logout } from "../controllers/auth.controller.js";
+import { login, signup, logout } from "../controllers/auth.controller.js";
 const router = express.Router();
 
-router.get("/login", login);
+router.post("/login", login);
 router.get("/logout", logout);
 
-router.post("/singup", singup);
+router.post("/signup", signup);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //to parse form data
 app.use("/api/auth", authRoutes);
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
